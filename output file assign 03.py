86e43c99530e16875e747451619db717f2bde981
keshavsingla015 <97163964+keshavsingla015@users.noreply.github.com>
2022-02-13 16:55:30 +0530

    Add files via upload

---
+++ output file assign 03.py
@@ -0,0 +1,101 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques01.py
+Question 1 
+Enter the string (preferably enter a single word) : KESHAV
+{'K': 1, 'E': 1, 'S': 1, 'H': 1, 'A': 1, 'V': 1}
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques02.py
+Question 2 
+Enter the day : 25
+Enter the month : 06
+Enter the year : 2023
+Date entered is 25/6/2023
+Next Date is 26/6/2023
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques02.py
+Question 2 
+Enter the day : 29
+Enter the month : 02
+Enter the year : 2008
+Date entered is 29/2/2008
+Next Date is 1/3/2008
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques3'.py
+Question 3
+Enter number of elements for squaring: 
+3
+Enter the number: 15
+Enter the number: 20
+Enter the number: 265
+Entered inputs: [15, 20, 265]
+Our required output is [(15, 225), (20, 400), (265, 70225)]
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques 4.py
+Question 4
+Enter your Grade points: 9
+Your Grade is A and  Excellent Performance. 
+
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques 5.py
+Question 5
+__OUTPUT__
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques 6.py
+QUESTION 6
+Enter SID: 21103018
+Enter Name: Keshav
+Do you want to enter another student details(Y or N): y
+Enter SID: 21103064
+Enter Name: Rahul
+Do you want to enter another student details(Y or N): y
+Enter SID: 20103056
+Enter Name: yuvraj
+Do you want to enter another student details(Y or N): n
+part A
+students details stored in dictionary are as :  {21103018: 'KESHAV', 21103064: 'RAHUL', 20103056: 'YUVRAJ'}
+part B
+Sorted names in alphabetical order are as:  {21103018: 'KESHAV', 21103064: 'RAHUL', 20103056: 'YUVRAJ'}
+part C
+sorted details according to SID are as:  {20103056: 'YUVRAJ', 21103018: 'KESHAV', 21103064: 'RAHUL'}
+part D
+Please Enter SID Of The Student You Want To Search: 21103018
+Student with entered SID is KESHAV
+
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques 07.py
+Question 7
+Enter number of terms of fibonacci series to print
+8
+0
+1
+1
+2
+3
+5
+8
+13
+Average of series is  4.125
+
+
+
+= RESTART: C:\Users\KESHAV SINGLA\Desktop\intro to pyhton assignment\assign 03\ques 8.py
+Question 8
+a. {1, 3, 5, 6, 8}
+b. {3, 6, 8, 9, 13, 17}
+c. {1, 2, 4, 5}
+d. {6, 7, 8, 9, 10}
+e. {10, 7}
